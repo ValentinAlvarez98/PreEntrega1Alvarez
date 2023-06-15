590ab5e3cf835bb31624b7d5d4b55b6a18dee645
Dne <valentinalvarez1998@gmail.com>
2023-06-15 16:45:25 -0300

    Se crea el método addToCart en la clase CartManager, que tomando como parámetro un id de carrito y un id de producto, agrega dicho id de producto y una cantidad a products dentro del carrito con el id ingresado. También se crea un endpoint POST en carts.router.js para la ejecución.

--- src/cartManager/cart_manager.js
+++ src/cartManager/cart_manager.js
@@ -69,6 +69,70 @@ class CartManager {
 
       };
 
+      // Se crea el método addToCart para agregar el id del producto al carrito, utilizando los productos creados en products.json.
+      addToCart = async (cartId, prodId) => {
+
+            // Se intenta ejecutar la consulta.
+            try {
+
+                  // Se verifica que exista el archivo JSON de productos.
+                  if (!fs.existsSync('../data/products.json')) return console.log('No existe ningún producto creado.');
+
+                  // Se verifica que exista el archivo JSON de carritos.
+                  if (!fs.existsSync(this.path)) return console.log('No existe ningún carrito creado.');
+
+                  // Se obtiene el array de productos del carrito.
+                  const cartData = await fs.promises.readFile(this.path, 'utf-8');
+                  this.cart = JSON.parse(cartData);
+
+                  // Se busca el carrito por su id.
+                  const cart = this.cart.find((cart) => cart.id === cartId);
+
+                  // Se verifica si existe el carrito.
+                  if (!cart) return console.log(`El id: ${cartId} ingresado, no pertenece a ningún carrito.`);
+
+                  // Se verifica si existe el producto.
+                  const productData = await fs.promises.readFile('../data/products.json', 'utf-8');
+                  const products = JSON.parse(productData);
+                  const product = products.find((product) => product.id === prodId);
+
+                  if (!product) return console.log(`El id: ${prodId} ingresado, no pertenece a ningún producto.`);
+
+                  // Se verifica si el producto ya existe en el carrito.
+                  const existingProduct = cart.products.find((item) => item.product === prodId);
+
+                  if (existingProduct) {
+
+                        // Si el producto ya existe en el carrito, se agrega otra unidad del mismo.
+                        existingProduct.quantity += 1;
+
+                  } else {
+
+                        // Si el producto no existe, se agrega al array de productos del carrito.
+                        const newProduct = {
+                              product: prodId,
+                              quantity: 1
+                        };
+
+                        cart.products.push(newProduct);
+
+                  };
+
+                  // Se escribe el archivo JSON con el nuevo producto agregado.
+                  await fs.promises.writeFile(this.path, JSON.stringify(this.cart, null, '\t'), 'utf-8');
+
+                  console.log("Producto agregado al carrito.");
+
+            } catch (error) {
+
+                  // Si hubo un error al ejecutar la consulta, se envía un mensaje de error.
+                  console.log('Error al ejecutar el método addToCart:', error);
+                  throw error;
+
+            };
+
+      };
+
 };
 
 // Se exporta la clase ProductManager.
--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -32,5 +32,73 @@ router.post('/', async (req, res) => {
 
 });
 
+// Se crea un endpoint GET para obtener todos los productos de un carrito por su id.
+router.get('/:cid', async (req, res) => {
+
+      // Se obtiene el id del carrito.
+      const id = req.params.cid;
+
+      // Se intenta ejecutar la consulta.
+      try {
+
+            // Se obtienen los productos del carrito utilizando el método getCartById de la clase CartManager.
+            const cart = await cartManager.getCartById(Number(id));
+
+            // Si no se encontró el carrito, se envía un mensaje de error.
+            if (!cart)
+                  return res.status(404).send({
+                        status: "error",
+                        error: `No se ha podido encontrar el carrito con el id: ${Number(id)}`
+                  });
+
+            // Si se encontró el carrito, se envía el carrito.
+            res.send(cart);
+
+      } catch (error) {
+
+            // Si hubo un error al ejecutar la consulta, se envía un mensaje de error.
+            res.status(500).send({
+                  status: "error",
+                  error: "Error al ejecutar la consulta."
+            });
+
+      };
+
+});
+
+// Se crea un endpoint POST para agregar un producto a un carrito.
+router.post('/:cid/product/:pid', async (req, res) => {
+
+      // Se obtiene el id del carrito.
+      const cartId = req.params.cid;
+
+      // Se obtiene el id del producto.
+      const productId = req.params.pid;
+
+      // Se intenta ejecutar la consulta.
+      try {
+
+            // Se agrega el producto al carrito utilizando el método addToCart de la clase CartManager.
+            const cart = await cartManager.addToCart(Number(cartId), Number(productId));
+
+            // Si se encontró el carrito y el producto, se envía el carrito.
+            res.status(201).send({
+                  status: "success",
+                  message: "Producto agregado al carrito.",
+                  cart
+            });
+
+      } catch (error) {
+
+            // Si hubo un error al ejecutar la consulta, se envía un mensaje de error.
+            res.status(500).send({
+                  status: "error",
+                  error: "Error al ejecutar la consulta."
+            });
+
+      };
+
+});
+
 // Se exporta el router.
 export default router;
